5596af72f426c06340a7dfa2ef4740caf0f62081
Shatabdiraut1013 <65498337+Shatabdiraut1013@users.noreply.github.com>
2021-05-09 07:25:13 +0530

    Add files via upload

---
+++ fracture.js
@@ -0,0 +1,36 @@
+import Abaloparatide from '../images/Abaloparatide.jpg'
+import AspirinandOxycodone from '../images/Aspirin and Oxycodone.jpg'
+import Metamizole from '../images/Metamizole.jpg'
+
+
+const fracture = [
+    {
+        imgsrc: "Abaloparatide",
+        title: "Abaloparatide",
+        indication: "Abaloparatide is prescribed to treat osteoporosis in postmenopausal women who are at a high risk for fracture.",
+        dosage: " The recommended adult dose is 80 mcg to be given subcutaneously (just under the skin) once a day",
+        sideEffects: "•  Gastrointestinal: Nausea, pain in upper abdominal area, constipation, upset stomach.",
+        price: 350,
+    },
+
+    {
+        imgsrc: "Aspirin and Oxycodone",
+        title: "Aspirin and Oxycodone ",
+        indication: "This combination medication is used to relieve moderate to severe pain.",
+        dosage: "The usual dosage is one tablet every 6 hours as needed for pain.",
+        sideEffects: " Most common : Lightheadedness, dizziness, drowsiness or sedation, nausea and vomiting.",
+        price: 705,
+    },
+
+
+    {
+        imgsrc: "Metamizole",
+        title: "Metamizole",
+        indication: "Metamizole is a painkiller used for the treatment of fever, headache, toothache, postoperative pain and other painful conditions.",
+        dosage: "The recommended oral adult dose for metamizole is 500 mg 3-4 times up to a maximum dose of 4000 mg/day.",
+        sideEffects: "Skin: Severe skin reactions like toxic epidermal necrolysis",
+        price: 432,
+    },
+]
+
+export default fracture;
